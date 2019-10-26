40c4b97822986f8fb83ab0735c5efb67e856a2c8
harshpatel <harshpatel@192.168.100.133>
2019-10-26 03:14:17 -0500

    First commit

---
+++ Swing1/src/App.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+//Basic Swing Applicaiotion
+public class App {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Hello World");
+		
+		frame,
+		frame.setVisible(true);
+	}
+
+}
